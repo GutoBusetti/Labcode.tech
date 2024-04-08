328961386a5b3ed096809cbbeec82e54684aed46
Augusto Busetti <gugu.busetti@gmail.com>
2024-04-07 23:12:41 -0300

    Create manutenção.js

---
+++ assets/js/manutenção.js
@@ -0,0 +1,10 @@
+const insta = document.querySelector('.insta');
+const zap = document.querySelector('.zap');
+
+insta.addEventListener('click', () => {
+    window.open('https://www.instagram.com/labcode_/', '_blank');
+});
+
+zap.addEventListener('click', () => {
+    window.open('https://wa.me/54991134598', '_blank');
+});
